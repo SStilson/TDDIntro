4bd74287ba9f2fcaa729c03eaba67e2be2669a22
SStilson <sstilson@hmc.edu>
2016-02-17 15:14:11 -0800

    Simplified factorial test

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -2,10 +2,10 @@ package com.thoughtworks.tddintro.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        if(i >= 0 && i <= 1) {
-            return 1;
-        } else if(i < 0) {
+        if(i < 0) {
             throw new IllegalArgumentException("i cannot be negative!");
+        } else if(i <= 1) {
+            return 1;
         } else {
             return i * compute(i-1);
         }
